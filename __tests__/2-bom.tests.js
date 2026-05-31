a30270faff566437c5973e7095facdec7c66a6de
Ангелина <gagylunaanglina@gmail.com>
2026-06-01 01:44:02 +0300

    Update 2-bom.tests.js

--- __tests__/2-bom.tests.js
+++ __tests__/2-bom.tests.js
@@ -1,18 +1,3 @@
-import '@testing-library/jest-dom';
-//import jest from 'jest-mock';
-import {jest} from '@jest/globals';
-import solution from '../solutions/2-bom.js';
-
-const assignMock = jest.fn();
-
-delete window.location;
-window.location = { assign: assignMock };
-
-afterEach(() => {
-  assignMock.mockClear();
-});
-
-test('application', () => {
-  expect(solution('https://yandex.ru')).toMatch(/Mozilla\/\d\.\d https:\/\/yandex\.ru/);
-});
-
+export default function solution(url) {
+  return `${navigator.userAgent} ${url}`;
+}
