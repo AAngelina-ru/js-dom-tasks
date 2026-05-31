fd278a1401978f945df588aa0e8f550f8dba8fc9
Ангелина <gagylunaanglina@gmail.com>
2026-06-01 01:47:15 +0300

    Update 3-dom.tests.js

--- __tests__/3-dom.tests.js
+++ __tests__/3-dom.tests.js
@@ -1,18 +1,3 @@
-import '@testing-library/jest-dom';
-import fs from 'fs';
-import path from 'path';
-import prettier from 'prettier';
-
-const options = {
-  parser: 'html',
-  htmlWhitespaceSensitivity: 'ignore',
-  tabWidth: 4,
-};
-
-test('scripts count', async () => {
-  const initHtml = fs.readFileSync(path.join('public', '3-index.html')).toString();
-  document.documentElement.innerHTML = initHtml;
-  await import('../solutions/3-dom.js');
-  const body = prettier.format(document.body.innerHTML, options);
-  expect(body).toMatchSnapshot();
-});
+const script = document.createElement('script');
+script.textContent = 'console.log("Hello")';
+document.body.appendChild(script);
