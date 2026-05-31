56cdc0c78a49e76f107b49ea209612e53f534342
Ангелина <gagylunaanglina@gmail.com>
2026-06-01 02:54:22 +0300

    Update 5-index.js

--- solutions/5-index.js
+++ solutions/5-index.js
@@ -2,3 +2,10 @@ import extractData from './5-search.js';
 
 const data = extractData(document);
 console.log(data);
+// 5-search.js должен экспортировать функцию, работающую с DOM
+import { JSDOM } from 'jsdom';
+import extractData from './5-search.js';
+
+const dom = new JSDOM('<!DOCTYPE html><html>...</html>');
+const data = extractData(dom.window.document);
+console.log(data);
