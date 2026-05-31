c17a538d8b2fd46421ac289f69241c2a903da97b
Ангелина <gagylunaanglina@gmail.com>
2026-06-01 02:51:40 +0300

    Update 4-index.js

--- solutions/4-index.js
+++ solutions/4-index.js
@@ -2,3 +2,7 @@ import extractData from './4-dom-hierarchy.js';
 
 const data = extractData(document.documentElement);
 console.log(data);
+import extractData from './4-dom-hierarchy.js';
+
+const data = extractData(document.documentElement);
+console.log(data);
